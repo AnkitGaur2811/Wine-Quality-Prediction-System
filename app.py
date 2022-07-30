5272d8b9c67f852b3f5fbd3874748f463705f862
Ankit Gaur <iamankitgaur@gmail.com>
2022-07-30 23:37:01 +0530

    Restructure for making web app

---
+++ app.py
@@ -0,0 +1,8 @@
+from matplotlib import backend_bases
+from sklearn.model_selection import LeaveOneGroupOut
+
+
+# Now making Webapp for the project using some new things i have Learnt 
+# Flask -> for backend
+# HTML,CSS, Jinja -> for frontend 
+# Heroku -> for deployment (if possible)
